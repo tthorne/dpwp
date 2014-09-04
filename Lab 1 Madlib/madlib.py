659e687e60dd1fb835cf4473519167e147a2f683
tthorne <tthorne@fullsail.edu>
2014-09-04 15:52:35 -0400

    Madlib

--- Lab 1 Madlib/madlib.py
+++ Lab 1 Madlib/madlib.py
@@ -7,16 +7,22 @@ Date: Thursday, September 4, 2014
 
 # While Loop
 i = 0
-while i<10:
-    
+while i<1:
+
     # The Variables
     name = raw_input('Enter a name: ')
     color = raw_input('Enter a color: ')
     animal = raw_input('Enter an animal: ')
-    fruit = raw_input('Pick a number between 1-5: ')
+    store = raw_input('Enter Store: (Pick a number between 0-2): ')
+    fruit = raw_input('Enter Fruit: (Pick a number between 1-5): ')
     fruit_cost = raw_input('Enter the cost of the fruit: ')
     many_fruit = raw_input('How many pieces of fruit do you want to buy? ')
     dollar_amount = raw_input('Enter a dollar amount: ')
+
+    #Array
+    store_name = ['Target','Walmart','Kmart']
+    list_store = int(store)
+    name_store = store_name[list_store]
 
     #Dictionary
     fruit_name = dict() # Dictionary
@@ -49,5 +55,5 @@ while i<10:
         cash = str(name) + ' needs $' + str(cash_needed) + ' more to purchase ' + str(list_fruit) + '.'
     else: cash = ' Continue to check out.'
 
-    print name + ' went to the store riding a ' + color + ' ' + animal + ' to buy some ' + list_fruit + '. ' + 'The fruit costs $' + fruit_cost + ' and ' + name + ' brought $' + dollar_amount + ' and wants to buy ' + many_fruit + ' ' + list_fruit + '. ' + str(total_fruit) + '. ' + str(cash)
+    print name + ' went to ' + str(name_store) +' riding a ' + color + ' ' + animal + ' to buy some ' + list_fruit + '. ' + 'The fruit costs $' + fruit_cost + ' and ' + name + ' brought $' + dollar_amount + ' and wants to buy ' + many_fruit + ' ' + list_fruit + '. ' + str(total_fruit) + '. ' + str(cash)
     i = i+1
