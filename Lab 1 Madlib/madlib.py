03a8131a57ef45c55b08a76f3fb1480c06d7a28b
tthorne <tthorne@fullsail.edu>
2014-09-04 13:04:00 -0400

    MadLib

---
+++ Lab 1 Madlib/madlib.py
@@ -0,0 +1,17 @@
+'''
+Student Name: Tara Thorne
+Student Number: #0003809521
+Assignment: Lab 1 Madlib
+Date: Thursday, September 4, 2014
+'''
+
+name = raw_input('Enter a name: ')
+location = raw_input('Enter a location: ')
+color = raw_input('Enter a color: ')
+animal = raw_input('Enter an animal: ')
+fruit = raw_input('Enter a fruit: ')
+number_fruit = raw_input('Enter a number: ')
+fruit_cost = raw_input('Enter the cost of the fruit: ')
+dollar_amount = raw_input('Enter a dollar amount: ')
+
+print name + ' went to ' + location + ' riding a ' + color + ' ' + animal + ' to buy some ' + fruit + '.'
