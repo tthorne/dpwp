a750cd7e8487575041bed87531c1354106dfc5db
tthorne <tthorne@fullsail.edu>
2014-09-04 14:41:56 -0400

    madlib

--- Lab 1 Madlib/madlib.py
+++ Lab 1 Madlib/madlib.py
@@ -18,8 +18,8 @@ if int(many_fruit) * int(fruit_cost) < int(dollar_amount):
     cost = int(fruit_cost)
     purchase = str(fruit)
     fruit_total = current - cost
-    total_fruit = name + ' can purchase ' + fruit_total
+    total_fruit = str(name) + ' can purchase ' + str(fruit_total) + ' ' + str(fruit) + "."
 else:
-    total_fruit = 'But without enough cash they'
+    total_fruit = str(name) + ' does not have enough cash to purchase ' + str(fruit)
 
-print name + ' went to the store riding a ' + color + ' ' + animal + ' to buy some ' + fruit + '. ' + 'The fruit costs $' + fruit_cost + ' and ' + name + ' brought $' + dollar_amount + ' and wants to buy ' + many_fruit + fruit + '. ' + str(total_fruit)
+print name + ' went to the store riding a ' + color + ' ' + animal + ' to buy some ' + fruit + '. ' + 'The fruit costs $' + fruit_cost + ' and ' + name + ' brought $' + dollar_amount + ' and wants to buy ' + many_fruit + ' ' + fruit + '. ' + str(total_fruit)
