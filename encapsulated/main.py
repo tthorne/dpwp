c7ba74dc1173a6773892e03dae32bbfd0f29066b
tthorne <tthorne@fullsail.edu>
2014-09-18 21:09:06 -0400

    3

--- encapsulated/main.py
+++ encapsulated/main.py
@@ -6,11 +6,15 @@ Date: Thursday, September 18, 2014
 GitHub URL: https://github.com/tthorne/dpwp/tree/master/Lab%201%20Madlib
 '''
 import webapp2
+from pages import Page
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        p = Page()
+        self.response.write(p.print_out())
+
         #Charles Registration
-        c = Transcript()
+        c = Registration()
         c.name = "Charles"
         c.address = "124 Main Street, Chambersburg, PA 17201"
         c.phone = "717-555-9545"
@@ -21,7 +25,7 @@ class MainHandler(webapp2.RequestHandler):
         self.response.write ("<strong>Name:</strong> " + c.name + "<br /><strong>Address:</strong> " + c.address + "<br /><strong>Phone:</strong> " + c.phone + "<br /><strong>Membership Level:</strong> " + c.level + "<br /><strong>Number of Horses Registered:</strong> " + str(c.horses) + "<br /><strong>Total Registration Fee:</strong> $" + str(c.final_price))
 
         #Megan Registration
-        m = Transcript()
+        m = Registration()
         m.name = "Megan"
         m.address = "567 Second Street, Chambersburg, PA 17201"
         m.phone = "717-555-9545"
@@ -32,7 +36,7 @@ class MainHandler(webapp2.RequestHandler):
         self.response.write ("<br><br><strong>Name:</strong> " + m.name + "<br /><strong>Address:</strong> " + m.address + "<br /><strong>Phone:</strong> " + m.phone + "<br /><strong>Membership Level:</strong> " + m.level + "<br /><strong>Number of Horses Registered:</strong> " + str(m.horses) + "<br /><strong>Total Registration Fee:</strong> $" + str(m.final_price))
 
         #Duke Registration
-        d = Transcript()
+        d = Registration()
         d.name = "Duke"
         d.address = "2905 Roland Avenue, Chambersburg, PA 17201"
         d.phone = "555-234-6789"
@@ -43,7 +47,7 @@ class MainHandler(webapp2.RequestHandler):
         self.response.write ("<br><br><strong>Name:</strong> " + d.name + "<br /><strong>Address:</strong> " + d.address + "<br /><strong>Phone:</strong> " + d.phone + "<br /><strong>Membership Level:</strong> " + d.level + "<br /><strong>Number of Horses Registered:</strong> " + str(d.horses) + "<br /><strong>Total Registration Fee:</strong> $" + str(d.final_price))
 
         #Jenn Registration
-        j = Transcript()
+        j = Registration()
         j.name = "Jennifer"
         j.address = "9012 Norland Avenue, Chambersburg, PA 17201"
         j.phone = "555-573-9753"
@@ -54,7 +58,7 @@ class MainHandler(webapp2.RequestHandler):
         self.response.write ("<br><br><strong>Name:</strong> " + j.name + "<br /><strong>Address:</strong> " + j.address + "<br /><strong>Phone:</strong> " + j.phone + "<br /><strong>Membership Level:</strong> " + j.level + "<br /><strong>Number of Horses Registered:</strong> " + str(j.horses) + "<br /><strong>Total Registration Fee:</strong> $" + str(j.final_price))
 
         #Tara's Registration
-        t = Transcript()
+        t = Registration()
         t.name = "Tara"
         t.address = "1242 Livermoore Road, Chambersburg, PA 17201"
         t.phone = "555-234-9874"
@@ -64,7 +68,7 @@ class MainHandler(webapp2.RequestHandler):
         t.calc_price()
         self.response.write ("<br><br><strong>Name:</strong> " + t.name + "<br /><strong>Address:</strong> " + t.address + "<br /><strong>Phone:</strong> " + t.phone + "<br /><strong>Membership Level:</strong> " + t.level + "<br /><strong>Number of Horses Registered:</strong> " + str(t.horses) + "<br /><strong>Total Registration Fee:</strong> $" + str(t.final_price))
 
-class Transcript(object):
+class Registration(object):
     def __init__(self):
         self.name = 0 #no underscores - public
         self.address = 0
---
+++ encapsulated/pages.py
@@ -0,0 +1,23 @@
+class Page(object):
+    def __init__(self):
+        self.title= "Welcome!"
+        self.css = "css/style.css"
+        self.head = """
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>{self.title}</title>
+        <link href="{self.css}" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        """
+
+        self.body = "Welcome to my OOP Python page!"
+        self.close = """
+    </body>
+</html>
+        """
+    def print_out(self):
+        all = self.head + self.body + self.close
+        all = all.format(**locals())
+        return all
