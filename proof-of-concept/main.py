13d6b849853d5426bc0fc401690e902369467fe5
tthorne <tthorne@fullsail.edu>
2014-09-20 18:19:53 -0400

    POC 2

--- proof-of-concept/main.py
+++ proof-of-concept/main.py
@@ -5,31 +5,9 @@ import json
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = FormPage()
-        p.inputs = [['item_id', 'text', 'Item Id'],['Submit', 'submit']]
+        p = ResultsPage()
+        p.inputs = [['item_id', 'text', 'Item Id ex.2060'],['Submit', 'submit']]
         self.response.write(p.print_out())
-
-        if self.request.GET: #only if there is a zip variable in the url
-            #get inro from api
-            item_id = self.request.GET['item_id']
-            url= "https://api.guildwars2.com/v1/item_details.json?item_id=" + item_id
-            #assemble the request
-            request = urllib2.Request(url)
-            #use the urllib2 to create and object from the api
-            opener = urllib2.build_opener()
-            #use the url to get a result - request info from API
-            result = opener.open(request)
-
-            #parse json
-            jsondoc = json.load(result)
-
-            name = jsondoc['name']
-            description = jsondoc['description']
-            type = jsondoc['type']
-            level = jsondoc['level']
-            rarity = jsondoc['rarity']
-            vendor_value = jsondoc['vendor_value']
-            self.response.write("<br/><strong>Item Name:</strong> " + name + "<br/><strong>Description:</strong>" + description + "<br/><strong>Type:</strong>" + type + "<br/><strong>Level:</strong>" + level + "<br/><strong>Rarity:</strong>" + rarity + "<br/><strong>Rarity:</strong>" + vendor_value)
-
 
 class Page(object): #borrowing stuff from the object class
     def __init__(self): #constructor
@@ -79,9 +57,44 @@ class FormPage(Page):
 
         print self._form_inputs
 
+class ResultsPage():
+    def __init__(self):
+        #constructor function for the super class
+        super(ResultsPage, self).__init__() #Page.__init__()
+        self._results = ''
+
+    @property
+    def results(self):
+        pass
+
+    @results.setter
+    def results(self):
+
+        if self.request.GET: #only if there is a zip variable in the url
+            #get inro from api
+            item_id = self.request.GET['item_id']
+            url= "https://api.guildwars2.com/v1/item_details.json?item_id=" + item_id
+            #assemble the request
+            request = urllib2.Request(url)
+            #use the urllib2 to create and object from the api
+            opener = urllib2.build_opener()
+            #use the url to get a result - request info from API
+            result = opener.open(request)
+
+            #parse json
+            jsondoc = json.load(result)
+
+            name = jsondoc['name']
+            description = jsondoc['description']
+            type = jsondoc['type']
+            level = jsondoc['level']
+            rarity = jsondoc['rarity']
+            vendor_value = jsondoc['vendor_value']
+            print self.results("<br/><strong>Item Name:</strong> " + name + "<br/><strong>Description:</strong>" + description + "<br/><strong>Type:</strong>" + type + "<br/><strong>Level:</strong>" + level + "<br/><strong>Rarity:</strong>" + rarity + "<br/><strong>Rarity:</strong>" + vendor_value)
+
     #polymorphism alert!!! -------- method overriding
     def print_out(self):
-        return self._head + self._body + self._form_open + self._form_inputs + self._form_close + self._close
+        return self._head + self._body + self._form_open + self._form_inputs + self._form_close + self._results + self._close
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
